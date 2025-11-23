3ef3985c5d8e4af94936ce1f1deca8980f9637af
beamartin27 <bmartin.ieu2023@student.ie.edu>
2025-11-23 13:10:01 +0100

    Adapted load_modules.sh to align with cluster

--- env/load_modules.sh
+++ env/load_modules.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-# TODO: adjust to actual cluster module names
-module purge
-module load gcc
-module load openmpi
-# If using Python later: module load python/3.x
+#!/usr/bin/env bash
+module --force purge
+module load StdEnv/2023
+module load gcc/12.3.0
+module load openmpi/4.1.5
